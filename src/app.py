ea7d445f83f8c69de0a12592bf1d8ddb60475c95
nrs20 <nrs5@njit.edu>
2024-04-05 23:46:26 -0400

    cleaning up

--- src/app.py
+++ src/app.py
@@ -13,6 +13,10 @@ import dash_bootstrap_components as dbc
 from dash import dash_table
 
 df = pd.read_csv('ProcessedTweets.csv')
+PAGE_SIZE = 6
+
+#to display an initially empty datatable.
+df_empty = pd.DataFrame(columns=['RawTweet'])
 
 
 #extract the months into a list to use for the dropdown
@@ -23,88 +27,107 @@ app=dash.Dash(__name__)
 app.layout = html.Div([
     html.Div([
         html.Div([
+            html.H2('Month:',  style={'display': 'inline-block', 
+                                      'margin-right': '10px', 'font-family': 'Georgia','font-size': '15px'}),
+
+            #dropdown for month
             dcc.Dropdown(
                 id='month',
                 options=[{'label': i, 'value': i} for i in months],
                 value=months[0],
                 style={'width': '50%','justifyContent': 'center','alignItems': 'center'}
             ),
-            html.H2('Sentiment Score', style={'display': 'inline-block', 'margin-right': '10px'}),
+            html.H2('Sentiment Score',  style={'display': 'inline-block', 'margin-right': '10px', 'font-family': 'Georgia','font-size': '15px'}),
+           #range slider for sentiment score
             dcc.RangeSlider(
-                id='sentiment_score',
+                id='sentiment_score_slider',
                 min=df['Sentiment'].min(),
                 max=df['Sentiment'].max(),
                 step=0.1,
-                marks={i: str(i) for i in range(-1, 2)},
-                value=[df['Sentiment'].min(), df['Sentiment'].max()],
-                className='black-slider'  # Add this line to set the color of the slider to black
+                marks={mark: str(float(mark)) for mark in range(int(df['Sentiment'].min()), int(df['Sentiment'].max()+1)) if mark!= 0},
+                value=[int(df['Sentiment'].min()), int(df['Sentiment'].max())],
+                className='black-slider'  
             ),
-            html.H2('Subjectivity Score', style={'display': 'inline-block', 'margin-right': '10px'}),
+            html.H2('Subjectivity Score', style={'display': 'inline-block', 'margin-right': '10px', 'font-family': 'Georgia','font-size': '15px'}),
+           #range slider for subjectivity score
             dcc.RangeSlider(
-                id='subjectivity_score',
+                id='subjectvity_score_slider',
                 min=df['Subjectivity'].min(),
                 max=df['Subjectivity'].max(),
                 step=0.1,
-                marks={i: str(i) for i in range(int(df['Subjectivity'].min()), int(df['Subjectivity'].max())+1)},
+                marks={mark: str(float(mark)) for mark in range(int(df['Subjectivity'].min()), int(df['Subjectivity'].max()+1))},
                 value=[df['Subjectivity'].min(), df['Subjectivity'].max()],
-                className='black-slider'  # Add this line to set the color of the slider to black
+                className='black-slider'  
             )
         ], style={'width': '70%'}, className='container')
     ], style={'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center'}),
     
     html.Div([
         dcc.Graph(id='sentiment')
+        
+        #make modebar appear vertically?     
     ], style={'width': '100%'}),
     
     html.Div([
         dash_table.DataTable(
             
             id='tweet_table',
-            style_table={'height': '300px', 'overflowY': 'auto', 'width': '75%', 'margin-left':'20px', 'alignItems': 'center', 'justifyContent': 'center'},
-            style_cell={'textAlign': 'center'}
+                    columns=[{"name": i, "id": i} for i in df_empty.columns],
+         data=df_empty.to_dict('records')  # This will be an empty list
+,
+           style_data={
+        'whiteSpace': 'normal',
+        'height': 'auto',
+    },
+            style_cell={'textAlign': 'center'},
+              page_current=0,
+    page_size=PAGE_SIZE,
+    page_action='custom'
         )
-    ])
+    ], style={'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center','margin-left':'50px'}),
+    
+    html.Div([
+    html.Div(id='table-container')
 ])
+])
+
+
+
 # Show the texts of selected tweets in a table format. Update the table based on scatter plot selections.
 @app.callback(
     dash.dependencies.Output('tweet_table', 'data'),
     [
-        dash.dependencies.Input('sentiment', 'selectedData')
+        dash.dependencies.Input('sentiment', 'selectedData'),
+        #add input for sliders and month dropdown
+        
+        dash.dependencies.Input('month', 'value'),
+        dash.dependencies.Input('sentiment_score_slider', 'value'),
+        dash.dependencies.Input('subjectvity_score_slider', 'value'),
+        dash.dependencies.Input('tweet_table', "page_current"),
+        dash.dependencies.Input('tweet_table', "page_size")
     ]
 )
-def display_selected_data(selectedData):
-    if selectedData is None:
+def display_selected_data(points_chosen, month, sentiment_score_slider, subjectvity_score_slider, page_current, page_size):
+    if points_chosen is not None: 
+           
+        lasso_points = points_chosen['points']
+        selected_indices = [point['pointIndex'] for point in lasso_points]
+    
+        print("SELECTED POINTS",lasso_points)
+        print("SELECTED INDICES",selected_indices)
+    
+        tableEntries = df.iloc[selected_indices][['RawTweet']]
+    
+         # Apply filters based on month, sentiment, and subjectivity scores (already applied in other callback.)
+        #filtered_entries = tableEntries[(df['Month'] == month) & (df['Sentiment'] >= sentiment_score_slider[0]) & (df['Sentiment'] <= sentiment_score_slider[1]) & (df['Subjectivity'] >= subjectvity_score_slider[0]) & (df['Subjectivity'] <= subjectvity_score_slider[1])]
+    
+         # Apply pagination (via dash documentation)
+        paginated_entries = tableEntries.iloc[page_current*page_size:(page_current+1)*page_size]
+    
+        data = paginated_entries.to_dict('records')
+        return data
+    else:
         return []
-    
-    selected_points = selectedData['points']
-    #print("Selected points:",selected_points)
-    selected_indices = [point['pointIndex'] for point in selected_points]
-    
-    #print(selected_indices)
-    
-    #use the x and y coordinates of the selected_indices for each point to get the corresponding indices in the dataframe
-    
-    #the x coordinate corresponds to 'Dimension 1' and the y coordinate corresponds to 'Dimension 2'
-    #grab the Raw Tweet matching the x and y coordinates of the selected points
-    
-    print("AQUI:",df.iloc[selected_indices][['RawTweet']])
-    
-    tableEntries = df.iloc[selected_indices][['RawTweet']]
-    #display selected_indices in a table format
-    
-    
-    
-    
-    #get the scores of the sentiment and subjectivity of the selected points
-    selected_tweets = df.iloc[selected_indices][['Sentiment', 'Subjectivity']]
-   # print("Selected tweets:",selected_tweets)
-    
-    #grab the tweets of the selected points
-    
-    #return the selected tweets and insert them into the table
-    data = tableEntries.to_dict('records')
-    return data
-
 
 
 # Implement callbacks to update the scatter plot based on the selected month and the ranges selected for sentiment and subjectivity.
@@ -113,18 +136,39 @@ def display_selected_data(selectedData):
     dash.dependencies.Output('sentiment', 'figure'),
     [
         dash.dependencies.Input('month', 'value'),
-        dash.dependencies.Input('sentiment_score', 'value'),
-        dash.dependencies.Input('subjectivity_score', 'value')
+        dash.dependencies.Input('sentiment_score_slider', 'value'),
+        dash.dependencies.Input('subjectvity_score_slider', 'value')
     ]
 )
-def update_graph(month, sentiment_score, subjectivity_score):
-    print(month, sentiment_score, subjectivity_score)
+
+def onGraphUpdate(month, sentiment_score_slider, subjectvity_score_slider):
+    print(month, sentiment_score_slider, subjectvity_score_slider)
     # Filter the data based on the selected month and the sentiment and subjectivity scores
-    filtered_df = df[(df['Month'] == month) & (df['Sentiment'] >= sentiment_score[0]) & (df['Sentiment'] <= sentiment_score[1]) & (df['Subjectivity'] >= subjectivity_score[0]) & (df['Subjectivity'] <= subjectivity_score[1])]
+    filtered_df = df[(df['Month'] == month) & (df['Sentiment'] >= sentiment_score_slider[0]) & (df['Sentiment'] <= sentiment_score_slider[1]) & (df['Subjectivity'] >= subjectvity_score_slider[0]) & (df['Subjectivity'] <= subjectvity_score_slider[1])]
     
     # Create the scatter plot
     fig = px.scatter(filtered_df, x='Dimension 1', y='Dimension 2')
     
+    # remove the axes labels
+    fig.update_xaxes(showticklabels=False)
+    fig.update_yaxes(showticklabels=False)
+    
+    #remove axis titles
+    fig.update_yaxes(title=None)
+    fig.update_xaxes(title=None)
+    
+    #remove gridlines like in the video
+    fig.update_yaxes(showgrid=False)
+    fig.update_xaxes(showgrid=False)
+    
+   # fig.update_xaxes(showline=False, showgrid=False)
+   
+   #make modebar appear vertically
+    fig.update_layout(
+        modebar={
+            'orientation': 'v'
+        }
+)
     return fig
 
 
